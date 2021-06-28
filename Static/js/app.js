72fc624ea39cc8326e4851bf07b23281abc750c8
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-06-28 00:19:06 -0400

    Added plot of OTUs

--- Static/js/app.js
+++ Static/js/app.js
@@ -11,7 +11,8 @@ d3.json('static/data/samples.json').then((importedData) => {
 
 
 // -----------------
-// Test Subject ID Numbers as drop down options
+// Test Subject ID Numbers as drop down options that will impact 
+// other elements of interactive charts based on  the selection
 // -----------------
 
 // Use D3 to connect to and read the JSON file ID Numbers (stored as "names")
@@ -26,10 +27,7 @@ d3.json('static/data/samples.json').then(({names}) => {
     renderData();
 });
 
-// -----------------
 // Define the function that will display demographics
-// -----------------
-
 function renderData() {
     
     // var seeMe = d3.select('select').node()
@@ -38,6 +36,7 @@ function renderData() {
     // Save the selected ID number to use for filtering 
     var sel = d3.select('select').node().value;
     console.log(`ID Number Selection: ${sel}`);
+    // console.log(typeof sel);
 
     // Use D3 to connect to and read JSON file demographics and samples
     d3.json('static/data/samples.json').then(({metadata, samples}) => {
@@ -48,11 +47,47 @@ function renderData() {
         // Append the keys and values for demographic info in the display panel
         d3.select('.panel-body').html('');
         Object.entries(demo).forEach(([key, val]) => {
-            d3.select('.panel-body').append('h5').text(key.toUpperCase() +': ' +val)
+            d3.select('.panel-body').append('h5').text(key.toUpperCase() +': ' + val);
+        
+        // Identify the top 10 OTUs for the selected individual; plot horizontal bar
+
+        // need to slice to only display top 10, need to reverse the order so bar chart reads top down
+        // need the OTU ids to be the labels along the left
+        d3.select('#bar').html('');
+        //var results = Object.entries(sample).forEach(([key, val]) => {
+          //  d3.select('#bar').sample.sort((a, b) => b.sample.sample_values - a.sample.sample_values);
+        
+        
+          //Object.entries(sample).forEach(([keyB, valB]) => {
+          //  console.log(sample);
+        
+        //var sortedSample = sample.sort((a, b) => b.sample.sample_values - a.sample.sample_values);
+        var trace = {
+            x: sample.sample_values,
+            y: sample.otu_ids,
+            type: "bar",
+            orientation: "h"
+        };
+        
+        var data = [trace];
+        
+        
+         var layout = {
+           title: "Top Ten OTUs for Selected Test Subject ID",
+           xaxis: { title: "Value" },
+           yaxis: { title: "OTU ID" }
+        };
+          
+        // Plot the chart with id "bar-plot"
+        Plotly.newPlot("bar", data, layout);
+
         })
     });
 };
 
+
 function optionChanged() {
     renderData();
 };
+
+
